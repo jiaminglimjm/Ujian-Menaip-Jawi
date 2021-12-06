f0a813c6664579b20ed989de151c3bbd0ed0e0f8
Jia Ming <jlim0176@student.monash.edu>
2021-12-06 18:09:20 +0800

    guna jarak levenshtein

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,34 @@ function randomInteger(min, max) { //from MDN
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// https://github.com/xwcoder/string-similarity-algorithm/blob/master/packages/string-similarity/src/levenshtein.ts
+function jarakLevenshtein (x, y) {
+  if (x === y) { return 0; }
+  if (!x.length) { return y.length; }
+  if (!y.length) { return x.length; }
+  if (x.length < y.length) { [x, y] = [y, x]; }
+
+  const matrix = [new Array(y.length + 1).fill('').map((_, i) => i)]
+  let index = 0
+
+  for (let i = 1; i <= x.length; i++) {
+    matrix[index = 1 - index] = [i]
+    for (let j = 1; j <= y.length; j++) {
+      if (x[i - 1] === y[j - 1]) {
+        matrix[index][j] = matrix[1 - index][j - 1]
+      } else {
+        matrix[index][j] = Math.min(
+          matrix[1 - index][j - 1] + 1, // 替换
+          matrix[1 - index][j] + 1, // 删除
+          matrix[index][j - 1] + 1 // 插入
+        )
+      }
+    }
+  }
+  return matrix[index][y.length]
+}
+
+
 $(document).ready(function() {
     $('div.bodyCurtain').fadeOut(500);
     for (let n = 0; n < amountOfWords; n++) {
@@ -57,13 +85,16 @@ function submitWord(word) {
     if(isCorrect)
     {
         correctWords += 1;
-        correctChars += (words[0].length);
+        correctChars += words[0].length;
     } else {
+        correctChars += Math.max(0, Math.min(words[0].length,
+            words[0].length - jarakLevenshtein(words[0], passageInput.value.replace(" ", ""))
+        ))
         showError();
     }
     document.getElementById('stats_correctWords').innerText = correctWords + "/" + totalWords;
     passageInput.value = '';
-    totalChars += (words[0].length) + 1;
+    totalChars += words[0].length;
     shiftWord();
     charAcc = (correctChars / totalChars) * 100;
     charAcc = parseFloat(charAcc).toFixed(1);
@@ -79,7 +110,7 @@ function showError() {
 function calcWPM() {
     //Count a word as 4 characters
     // https://brunei-linguistics.blogspot.com/2017/08/word-length-in-malay-and-english.html
-    let wordsEstimate = correctChars / 6.5;
+    let wordsEstimate = correctChars / 6;
     WPM = (wordsEstimate / timeElapsed)*60;
     WPM = parseFloat(WPM).toFixed(1);
     document.getElementById('stats_wordsPerMinute').innerText = WPM;
@@ -137,14 +168,13 @@ passageInput.addEventListener('input', () => {
         beginTest();
     }
     if(passageInput.value == words[0] || passageInput.value == words[0] + " ") {
-        $("div.word:nth-child(1)").css('color', 'rgb(0,255,0)');
+        $("div.word:nth-child(1)").css('color', 'rgb(0,232,0)');
     } else {
         $("div.word:nth-child(1)").css('color', 'white');
     }
     const lastChar = passageInput.value.charAt(passageInput.value.length - 1);
     if(lastChar == ' ')
     {
-        correctChars += 1;
         submitWord(passageInput.value);
         updateWordsRemaining();
     }
